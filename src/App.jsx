07235e4f3cb124e59db22ed83fbd0c39553b46cc
taiyeba-software <islamtaiyeba38@gmail.com>
2025-06-22 04:07:02 +0600

    tailwind project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound";
+
+function App() {
+    
+  return(  
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="*" element={<NotFound/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='min-h-screen container'>
+      
+    {/*theme toggle */}
+
+    {/*background effects */}
+
+    {/*NAVBAR  */}
+
+    {/*main content */}
+
+
+    {/*footer */}
+
+
+    </div>
+  )
+}
+
+export default Home
